581b2c587bc9834f9484af00eca89359f051cabb
yeondori <yeonseo.jung0@gmail.com>
2023-11-12 01:40:52 +0900

    feat: 정적 팩토리 메서드 from 생성

--- src/main/java/christmas/OrderManager.java
+++ src/main/java/christmas/OrderManager.java
@@ -3,7 +3,12 @@ package christmas;
 import java.util.HashMap;
 
 public class OrderManager {
-    private final HashMap<Menu, Integer> orderHistory = new HashMap<>();
+    private final HashMap<Menu, Integer> orders = new HashMap<>();
 
+    private OrderManager(MenuBoard menuBoard) {
+    }
 
+    public static OrderManager from(MenuBoard menuBoard) {
+        return new OrderManager(menuBoard);
+    }
 }
