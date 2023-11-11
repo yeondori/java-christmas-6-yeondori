e42c61963ac34ab11631cb3c33192e103e2699ee
yeondori <yeonseo.jung0@gmail.com>
2023-11-12 02:13:08 +0900

    feat: validateQuantity 생성

--- src/main/java/christmas/OrderManager.java
+++ src/main/java/christmas/OrderManager.java
@@ -1,6 +1,8 @@
 package christmas;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
 public class OrderManager {
     private final HashMap<Menu, Integer> orders;
@@ -12,15 +14,31 @@ public class OrderManager {
     }
 
     public static OrderManager from(MenuBoard menuBoard) {
-        return new OrderManager(new HashMap<>(),menuBoard);
+        return new OrderManager(new HashMap<>(), menuBoard);
     }
 
     public void receiveOrders(HashMap<String, Integer> orderInput) {
     }
 
-    public void validateMenu(String name) {
-        if(!menuBoard.hasMenu(name)) {
+    public void validateMenu(Set<String> orderMenus) {
+        for(String menu : orderMenus) {
+            if (!menuBoard.hasMenu(menu)) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+        }
+    }
+
+    public void validateQuantity(Collection<Integer> orderQuantity) {
+        int totalQuantity = orderQuantity.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        if (totalQuantity < 1) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        if (totalQuantity > 20) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 최대 20개까지 주문 가능합니다. 다시 입력해 주세요.");
         }
     }
 }
