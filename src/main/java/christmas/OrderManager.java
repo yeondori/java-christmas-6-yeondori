f4817fcf4226276b8cf21e108efb587cf87ff994
yeondori <yeonseo.jung0@gmail.com>
2023-11-12 01:50:29 +0900

    feat: validateMenu 생성

--- src/main/java/christmas/OrderManager.java
+++ src/main/java/christmas/OrderManager.java
@@ -3,12 +3,24 @@ package christmas;
 import java.util.HashMap;
 
 public class OrderManager {
-    private final HashMap<Menu, Integer> orders = new HashMap<>();
+    private final HashMap<Menu, Integer> orders;
+    private final MenuBoard menuBoard;
 
-    private OrderManager(MenuBoard menuBoard) {
+    public OrderManager(HashMap<Menu, Integer> orders, MenuBoard menuBoard) {
+        this.orders = orders;
+        this.menuBoard = menuBoard;
     }
 
     public static OrderManager from(MenuBoard menuBoard) {
-        return new OrderManager(menuBoard);
+        return new OrderManager(new HashMap<>(),menuBoard);
+    }
+
+    public void receiveOrders(HashMap<String, Integer> orderInput) {
+    }
+
+    public void validateMenu(String name) {
+        if(!menuBoard.hasMenu(name)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
