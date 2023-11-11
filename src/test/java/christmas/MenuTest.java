06552ca907412e222d4afaed34487d23bf057bc6
yeondori <yeonseo.jung0@gmail.com>
2023-11-11 12:11:12 +0900

    refactor: 메서드명 변경 및 @DisplayName 어노테이션 추가

--- src/test/java/christmas/MenuTest.java
+++ src/test/java/christmas/MenuTest.java
@@ -1,26 +1,31 @@
 package christmas;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MenuTest {
+    @DisplayName("메뉴에 존재하는 음식이면 true를 반환한다")
     @Test
-    public void 메뉴에_있으면_true를_반환한다() {
+    public void hasMenuTrueCase() {
         Menu menu = new Menu();
 
         assertThat(menu.hasMenu("양송이수프")).isTrue();
     }
 
+
+    @DisplayName("메뉴에 존재하지 않는 음식이면 false를 반환한다")
     @Test
-    public void 메뉴에_없으면_false를_반환한다() {
+    public void hasMenuFalseCase() {
         Menu menu = new Menu();
 
         assertThat(menu.hasMenu("알리오올리오")).isFalse();
     }
 
+    @DisplayName("메뉴의 카테고리를 반환한다")
     @Test
-    public void 메뉴의_카테고리를_반환한다() {
+    public void findCategory() {
         Menu menu = new Menu();
 
         assertThat(menu.findCategory("티본스테이크")).isEqualTo(Category.MAIN);
