6cc8f6222ba487f35242c72174f2be62603880b1
yeondori <yeonseo.jung0@gmail.com>
2023-11-13 00:26:03 +0900

    feat: getChristmasDiscount 기능 추가

--- src/main/java/christmas/DiscountPolicy.java
+++ src/main/java/christmas/DiscountPolicy.java
@@ -1,5 +1,16 @@
 package christmas;
 
-public class DiscountPolicy {
+import java.util.Arrays;
+import java.util.List;
 
+public class DiscountPolicy {
+    private final List<Integer> weekend = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+    private final List<Integer> specialDay = Arrays.asList(3, 10, 17, 24, 25, 31);
+
+    public int getChristmasDiscount(int date) {
+        if (date >= 1 && date <= 25) {
+            return 1000 + (date - 1) * 100;
+        }
+        return 0;
+    }
 }
