73bd4eb1c621cd7df21cd6ddedf2c7ab411d071f
yeondori <yeonseo.jung0@gmail.com>
2023-11-13 17:39:11 +0900

    feat: createOrderOf 기능 추가

--- src/main/java/christmas/Order.java
+++ src/main/java/christmas/Order.java
@@ -1,14 +1,38 @@
 package christmas;
 
 public class Order {
+    private static final MenuBoard menuBoard = MenuBoard.loadMenu();
     private final Category category;
     private final String menuName;
     private final int quantity;
 
-    public Order(Category category, String menuName, int quantity) {
+    private Order(Category category, String menuName, int quantity) {
         this.category = category;
         this.menuName = menuName;
         this.quantity = quantity;
     }
 
+    public static Order createOrderOf(String menuName, int quantity) {
+        validateMenu(menuName);
+        validateQuantity(quantity);
+
+        Category category = menuBoard.findCategory(menuName);
+
+        return new Order(category, menuName, quantity);
+    }
+
+    private static void validateMenu(String menuName) {
+        if (!menuBoard.hasMenu(menuName)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static void validateQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        if (quantity > 20) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 최대 20개까지 주문 가능합니다. 다시 입력해 주세요.");
+        }
+    }
 }
