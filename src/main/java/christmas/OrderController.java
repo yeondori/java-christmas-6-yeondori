ddb3c3f01ccd058fe827e4271b31d72eebf67fe3
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 00:06:52 +0900

    feat: receiveOrders 기능 추가

--- src/main/java/christmas/OrderController.java
+++ src/main/java/christmas/OrderController.java
@@ -1,4 +1,20 @@
 package christmas;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class OrderController {
+    public static List<Order> receiveOrders(Map<String, Integer> orderInput) {
+        List<Order> orders = new ArrayList<>();
+        orderInput.forEach((menuName, quantity) -> {
+            orders.add(Order.createOrderOf(menuName, quantity));
+        });
+
+        validateOrders(orders);
+        return orders;
+    }
+
+    private static void validateOrders(List<Order> orders) {
+    }
 }
