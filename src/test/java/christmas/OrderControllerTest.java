0a3dacb94458172f18312d4f95d42051719469c7
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 01:20:19 +0900

    feat: OrderControllerTest 생성

---
+++ src/test/java/christmas/OrderControllerTest.java
@@ -0,0 +1,16 @@
+package christmas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @Test
+    void receiveOrders() {
+    }
+
+    @Test
+    void getTotalPrice() {
+    }
+}
