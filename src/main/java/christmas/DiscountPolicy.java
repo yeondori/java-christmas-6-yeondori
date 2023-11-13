c97594688486ccaa6c827160708275327c23eb4f
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 03:25:14 +0900

    refactor: GIFT_CONDITION -> GIFT.getPriceCondition() 변경

--- src/main/java/christmas/DiscountPolicy.java
+++ src/main/java/christmas/DiscountPolicy.java
@@ -1,9 +1,9 @@
 package christmas;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import static christmas.Discount.*;
 
 public class DiscountPolicy {
     private final List<Integer> weekend = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
@@ -11,35 +11,35 @@ public class DiscountPolicy {
 
     public int getChristmasDiscount(int date) {
         if (date >= 1 && date <= 25) {
-            return 1000 + (date - 1) * 100;
+            return CHRISTMAS.getBaseDiscount() + (date - 1) * CHRISTMAS.getAdditionalDiscount();
         }
         return 0;
     }
 
     public int getSpecialDayDiscount(int date) {
         if (specialDay.contains(date)) {
-            return 1000;
+            return SPECIAL.getBaseDiscount();
         }
         return 0;
     }
 
     public int getWeekdayDiscount(int date, int dessertQuantity) {
         if (!weekend.contains(date)) {
-            return dessertQuantity * 2023;
+            return dessertQuantity * WEEKDAY.getAdditionalDiscount();
         }
         return 0;
     }
 
     public int getWeekendDiscount(int date, int mainQuantity) {
         if (weekend.contains(date)) {
-            return mainQuantity * 2023;
+            return mainQuantity * WEEKEND.getAdditionalDiscount();
         }
         return 0;
     }
 
     public int getGiftDiscount(int totalPrice) {
-        if (totalPrice>=120_000) {
-            return 25000;
+        if (totalPrice >= GIFT.getPriceCondition()) {
+            return GIFT.getBaseDiscount();
         }
         return 0;
     }
