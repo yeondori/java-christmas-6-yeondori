3d347b85dd48722323b2db12391ac88837b5e2e9
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 01:30:10 +0900

    feat: receiveOrders 테스트(주문가능 수량을 초과하는 경우)

--- src/test/java/christmas/OrderControllerTest.java
+++ src/test/java/christmas/OrderControllerTest.java
@@ -1,13 +1,25 @@
 package christmas;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderControllerTest {
 
+    @DisplayName("총주문수량이 20을 초과하는 경우 예외 처리한다.")
     @Test
-    void receiveOrders() {
+    void receiveOrdersMoreThanMaxQuantity() {
+        Map<String, Integer> orderInput = new HashMap<>();
+        orderInput.put("양송이수프", 1);
+        orderInput.put("티본스테이크", 20);
+
+        assertThatThrownBy(() -> OrderController.receiveOrders(orderInput))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
