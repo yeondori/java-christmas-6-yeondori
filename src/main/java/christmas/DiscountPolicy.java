c3138fb8498c97ec0228b188aae049ea22e6bb42
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 03:37:09 +0900

    feat: EventCalendar 상수로 대치

--- src/main/java/christmas/DiscountPolicy.java
+++ src/main/java/christmas/DiscountPolicy.java
@@ -1,37 +1,33 @@
 package christmas;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static christmas.Discount.*;
+import static christmas.EventCalendar.*;
 
 public class DiscountPolicy {
-    private final List<Integer> weekend = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
-    private final List<Integer> specialDay = Arrays.asList(3, 10, 17, 24, 25, 31);
 
     public int getChristmasDiscount(int date) {
-        if (date >= 1 && date <= 25) {
+        if (date >= CHRISTMAS_START && date <= CHRISTMAS_END) {
             return CHRISTMAS.getBaseDiscount() + (date - 1) * CHRISTMAS.getAdditionalDiscount();
         }
         return 0;
     }
 
     public int getSpecialDayDiscount(int date) {
-        if (specialDay.contains(date)) {
+        if (SPECIAL_DAY.contains(date)) {
             return SPECIAL.getBaseDiscount();
         }
         return 0;
     }
 
     public int getWeekdayDiscount(int date, int dessertQuantity) {
-        if (!weekend.contains(date)) {
+        if (!WEEKEND_DAY.contains(date)) {
             return dessertQuantity * WEEKDAY.getAdditionalDiscount();
         }
         return 0;
     }
 
     public int getWeekendDiscount(int date, int mainQuantity) {
-        if (weekend.contains(date)) {
+        if (WEEKEND_DAY.contains(date)) {
             return mainQuantity * WEEKEND.getAdditionalDiscount();
         }
         return 0;
