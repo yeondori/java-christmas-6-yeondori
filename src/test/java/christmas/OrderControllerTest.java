076c51fd4d9d8125f72cd249730f8c01716c0b75
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 01:31:29 +0900

    feat: receiveOrders 테스트(음료만 주문하는 경우)

--- src/test/java/christmas/OrderControllerTest.java
+++ src/test/java/christmas/OrderControllerTest.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class OrderControllerTest {
 
@@ -17,6 +16,17 @@ class OrderControllerTest {
         Map<String, Integer> orderInput = new HashMap<>();
         orderInput.put("양송이수프", 1);
         orderInput.put("티본스테이크", 20);
+
+        assertThatThrownBy(() -> OrderController.receiveOrders(orderInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("음료만 주문하는 경우 예외 처리한다.")
+    @Test
+    void receiveOrdersOnlyDrinks() {
+        Map<String, Integer> orderInput = new HashMap<>();
+        orderInput.put("제로콜라", 1);
+        orderInput.put("샴페인", 2);
 
         assertThatThrownBy(() -> OrderController.receiveOrders(orderInput))
                 .isInstanceOf(IllegalArgumentException.class);
