79a4aaebce6c3c74ea871bd508df96a7d22de634
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 01:38:22 +0900

    refactor: MAX_QUANTITY 상수 처리

--- src/test/java/christmas/OrderControllerTest.java
+++ src/test/java/christmas/OrderControllerTest.java
@@ -4,18 +4,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OrderControllerTest {
 
-    @DisplayName("총주문수량이 20을 초과하는 경우 예외 처리한다.")
+    private final int MAX_QUANTITY = 20;
+
+    @DisplayName("총 주문가능 수량(20)을 초과하는 경우 예외 처리한다.")
     @Test
     void receiveOrdersMoreThanMaxQuantity() {
         Map<String, Integer> orderInput = new HashMap<>();
         orderInput.put("양송이수프", 1);
-        orderInput.put("티본스테이크", 20);
+        orderInput.put("티본스테이크", MAX_QUANTITY);
 
         assertThatThrownBy(() -> OrderController.receiveOrders(orderInput))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -32,7 +36,18 @@ class OrderControllerTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("주문내역을 통해 총금액을 반환한다.")
     @Test
     void getTotalPrice() {
+        Map<String, Integer> orderInput = new HashMap<>();
+        orderInput.put("양송이수프", 1);
+        orderInput.put("제로콜라", 2);
+
+        int expectedPrice = MenuBoard.loadMenu().findPrice("양송이수프") * 1
+                + MenuBoard.loadMenu().findPrice("제로콜라") * 2;
+
+        List<Order> orders = OrderController.receiveOrders(orderInput);
+
+        assertThat(OrderController.getTotalPrice(orders)).isEqualTo(expectedPrice);
     }
 }
