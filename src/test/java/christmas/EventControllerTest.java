87b24ab944cf04dc5cdedc070bc8193dcd456542
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 04:10:19 +0900

    feat: EventControllerTest 생성

---
+++ src/test/java/christmas/EventControllerTest.java
@@ -0,0 +1,16 @@
+package christmas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventControllerTest {
+
+    @Test
+    void getEventBadge() {
+    }
+
+    @Test
+    void getTotalBenefitPrice() {
+    }
+}
