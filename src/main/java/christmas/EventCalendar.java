1fe6cd74389fd9dd656beb6f73f7612a77feb2fc
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 03:34:44 +0900

    feat: EventCalendar 상수 추가

---
+++ src/main/java/christmas/EventCalendar.java
@@ -0,0 +1,11 @@
+package christmas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EventCalendar {
+    public static final List<Integer> WEEKEND = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+    public static final List<Integer> SPECIAL_DAY = Arrays.asList(3, 10, 17, 24, 25, 31);
+    public static final int CHRISTMAS_START = 1;
+    public static final int CHRISTMAS_END = 25;
+}
