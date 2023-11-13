5290a1c3ae7e3c5a93c35cfca599f121e69f227c
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 01:45:25 +0900

    feat: 생성자 및 정적 팩토리 메서드 추가

--- src/main/java/christmas/EventController.java
+++ src/main/java/christmas/EventController.java
@@ -1,5 +1,19 @@
 package christmas;
 
-public class EventController {
+import java.util.List;
 
+public class EventController {
+    private final DiscountPolicy discountPolicy;
+    private final OrderController orderController;
+    private final List<Order> orders;
+
+    private EventController(DiscountPolicy discountPolicy, OrderController orderController, List<Order> orders) {
+        this.discountPolicy = discountPolicy;
+        this.orderController = orderController;
+        this.orders = orders;
+    }
+
+    public EventController from(List<Order> receiveOrders) {
+        return new EventController(discountPolicy, orderController, receiveOrders);
+    }
 }
