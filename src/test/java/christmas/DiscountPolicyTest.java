0b30ca8973252e4051279bf505751cf0f7ccc926
yeondori <yeonseo.jung0@gmail.com>
2023-11-13 14:42:43 +0900

    feat: getWeekendDiscountOnMainOrder 테스트(주말에 메인을 주문한 경우)

--- src/test/java/christmas/DiscountPolicyTest.java
+++ src/test/java/christmas/DiscountPolicyTest.java
@@ -10,7 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DiscountPolicyTest {
     private final List<Integer> specialDay = Arrays.asList(3, 10, 17, 24, 25, 31);
-    private final List<Integer> weekend = Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30);
+    private final int WEEKDAY = 3;
+    private final int WEEKEND = 2;
 
     @DisplayName("크리스마스 기간에는 크리스마스 할인을 받을 수 있다.")
     @Test
@@ -41,16 +42,25 @@ class DiscountPolicyTest {
         }
     }
 
+    @DisplayName("특정 날짜가 아니면 특별 할인을 받을 수 없다.")
+    @Test
+    void getNoSpecialDiscountOnNonSpecialDay() {
+        DiscountPolicy discountPolicy = new DiscountPolicy();
+
+        int specialDiscount = discountPolicy.getSpecialDayDiscount(1);
+        assertThat(specialDiscount).isEqualTo(0);
+
+    }
+
     @DisplayName("평일에 디저트를 주문하면 평일 할인을 받을 수 있다.")
     @Test
     void getWeekdayDiscountOnDessertOrder() {
         DiscountPolicy discountPolicy = new DiscountPolicy();
 
-        int date = 3;
         int dessertQuantity = 1;
-        int weekdayDiscount = discountPolicy.getWeekdayDiscount(date, dessertQuantity);
+        int weekdayDiscount = discountPolicy.getWeekdayDiscount(WEEKDAY, dessertQuantity);
 
-        assertThat(weekdayDiscount).isEqualTo(2023);
+        assertThat(weekdayDiscount).isEqualTo(2023 * dessertQuantity);
     }
 
     @DisplayName("평일에 디저트를 주문하지 않으면 평일 할인을 받을 수 없다.")
@@ -58,10 +68,20 @@ class DiscountPolicyTest {
     void getNoWeekdayDiscountOnNonDessertOrder() {
         DiscountPolicy discountPolicy = new DiscountPolicy();
 
-        int date = 3;
-        int dessertQuantity = 0;
-        int weekdayDiscount = discountPolicy.getWeekdayDiscount(date, dessertQuantity);
+        int noDessert = 0;
+        int weekdayDiscount = discountPolicy.getWeekdayDiscount(WEEKDAY, noDessert);
 
         assertThat(weekdayDiscount).isEqualTo(0);
+    }
+
+    @DisplayName("주말에 메인을 주문하면 주말 할인을 받을 수 있다.")
+    @Test
+    void getWeekendDiscountOnMainOrder() {
+        DiscountPolicy discountPolicy = new DiscountPolicy();
+
+        int mainQuanitty = 2;
+        int weekendDiscount = discountPolicy.getWeekendDiscount(WEEKEND, mainQuanitty);
+
+        assertThat(weekendDiscount).isEqualTo(2023 * mainQuanitty);
     }
 }
