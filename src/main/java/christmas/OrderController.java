0427b6d5eb45f7719a7a8d7c3ca62a6dba668797
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 00:37:24 +0900

    feat: getOrders 기능 추가

--- src/main/java/christmas/OrderController.java
+++ src/main/java/christmas/OrderController.java
@@ -3,18 +3,20 @@ package christmas;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import static christmas.Category.*;
+import static christmas.Category.DRINK;
 
 public class OrderController {
+
     public static List<Order> receiveOrders(Map<String, Integer> orderInput) {
-        List<Order> orders = new ArrayList<>();
+        List<Order> receiveOrders = new ArrayList<>();
         orderInput.forEach((menuName, quantity) -> {
-            orders.add(Order.createOrderOf(menuName, quantity));
+            receiveOrders.add(Order.createOrderOf(menuName, quantity));
         });
 
-        validateOrders(orders);
-        return orders;
+        validateOrders(receiveOrders);
+        return receiveOrders;
     }
 
     private static void validateOrders(List<Order> orders) {
@@ -28,7 +30,7 @@ public class OrderController {
                 .mapToInt(Order::getQuantity)
                 .sum();
 
-        if (totalQuantity>20) {
+        if (totalQuantity > 20) {
             throw new IllegalArgumentException("[ERROR] 메뉴는 최대 20개까지 주문 가능합니다. 다시 입력해 주세요.");
         }
     }
@@ -51,14 +53,19 @@ public class OrderController {
                 .distinct()
                 .toList();
 
-        if(categories.size()==1 && categories.contains(DRINK)) {
+        if (categories.size() == 1 && categories.contains(DRINK)) {
             throw new IllegalArgumentException("[ERROR] 음료만 주문하는 것은 불가합니다. 다시 입력해 주세요.");
         }
     }
 
-    public static int calculateTotalPrice(List<Order> orders) {
+    public static int getTotalPrice(List<Order> orders) {
         return orders.stream()
                 .mapToInt(order -> Order.calculateOrderPrice(order.getMenuName()))
                 .sum();
+    }
+
+    public static Map<String, Integer> getOrders(List<Order> orders) {
+        return orders.stream()
+                .collect(Collectors.toMap(Order::getMenuName, Order::getQuantity));
     }
 }
