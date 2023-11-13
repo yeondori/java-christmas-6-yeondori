2e96add7e2901030741d1ac1c91645a6b4eaca96
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 03:46:48 +0900

    feat: getTotalBenefits 기능 추가

--- src/main/java/christmas/EventController.java
+++ src/main/java/christmas/EventController.java
@@ -1,6 +1,12 @@
 package christmas;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static christmas.Category.*;
+import static christmas.Discount.*;
 
 public class EventController {
     private final DiscountPolicy discountPolicy;
@@ -15,5 +21,18 @@ public class EventController {
 
     public EventController from(List<Order> receiveOrders) {
         return new EventController(discountPolicy, orderController, receiveOrders);
+    }
+
+    public Map<Discount, Integer> getTotalBenefits(int date) {
+        Map<Category, Integer> quantityByCategory = OrderController.getQuantityByCategory(orders);
+
+        Map<Discount, Integer> totalBenefits = new HashMap<>();
+        totalBenefits.put(CHRISTMAS, discountPolicy.getChristmasDiscount(date));
+        totalBenefits.put(SPECIAL, discountPolicy.getSpecialDayDiscount(date));
+        totalBenefits.put(WEEKDAY, discountPolicy.getWeekdayDiscount(date, quantityByCategory.get(DESSERT)));
+        totalBenefits.put(WEEKEND, discountPolicy.getWeekendDiscount(date, quantityByCategory.get(MAIN)));
+        totalBenefits.put(GIFT, discountPolicy.getGiftDiscount(OrderController.getTotalPrice(orders)));
+
+        return totalBenefits;
     }
 }
