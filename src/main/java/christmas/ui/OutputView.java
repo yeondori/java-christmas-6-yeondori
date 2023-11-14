993873113f18f03bc8d7becbdcd15de4ae4e129b
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 17:15:35 +0900

    feat: 메서드명 변경 및 기능 추가 (printMenu -> printOrder, 주문 금액 출력)

--- src/main/java/christmas/ui/OutputView.java
+++ src/main/java/christmas/ui/OutputView.java
@@ -11,13 +11,17 @@ public class OutputView {
         System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
     }
 
-    public void printMenu(List<Order> orders) {
+    public void printOrder(List<Order> orders) {
         System.out.println("<주문 메뉴>");
         Map<String, Integer> orderMenus = OrderController.getOrders(orders);
 
         orderMenus.entrySet().stream()
                 .map(entry -> entry.getKey() + " " + entry.getValue() + "개")
                 .forEach(System.out::println);
-        System.out.println();
+
+        System.out.println("\n<할인 전 총주문 금액>");
+        System.out.println(OrderController.getTotalPrice(orders));
     }
+
+    public void print
 }
