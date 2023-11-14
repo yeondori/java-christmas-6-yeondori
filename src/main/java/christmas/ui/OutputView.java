58cca9235e9f7844417bcb2a3f1dbb9d463f2294
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 17:34:19 +0900

    feat: printBenefitsDetail 기능 추가

--- src/main/java/christmas/ui/OutputView.java
+++ src/main/java/christmas/ui/OutputView.java
@@ -4,6 +4,7 @@ import christmas.discount.Discount;
 import christmas.event.EventController;
 import christmas.order.Order;
 import christmas.order.OrderController;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
 import java.util.Map;
@@ -22,19 +23,41 @@ public class OutputView {
                 .forEach(System.out::println);
 
         System.out.println("\n<할인 전 총주문 금액>");
-        System.out.println(OrderController.getTotalPrice(orders));
+        String formattedPrice = String.format("%,d원", OrderController.getTotalPrice(orders));
+        System.out.println(formattedPrice);
     }
 
     public void printBenefits(List<Order> orders, int date) {
-        System.out.println("\n<증정 메뉴>");
-
         EventController eventController = EventController.from(orders);
         Map<Discount, Integer> totalBenefits = eventController.getTotalBenefits(date);
 
         printGift(totalBenefits);
+        printBenefitsDetail(totalBenefits);
+
+    }
+
+    private static void printBenefitsDetail(Map<Discount, Integer> totalBenefits) {
+        System.out.println("<혜택 내역>");
+        boolean hasDiscount = false;
+
+        for (Map.Entry<Discount, Integer> entry : totalBenefits.entrySet()) {
+            int discountAmount = entry.getValue();
+
+            if (discountAmount != 0) {
+                hasDiscount = true;
+
+                System.out.printf("%s 할인: %,d원%n", entry.getKey().name(), discountAmount);
+            }
+        }
+
+        if (!hasDiscount) {
+            System.out.println("없음");
+        }
     }
 
     private static void printGift(Map<Discount, Integer> totalBenefits) {
+        System.out.println("\n<증정 메뉴>");
+
         Integer giftDiscount = totalBenefits.get(Discount.GIFT);
         if (giftDiscount != 0) {
             System.out.println("샴페인 1개");
