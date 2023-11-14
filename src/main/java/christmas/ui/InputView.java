88bbf6dbb23b41fce44bac77d6baa96ad5fd92b2
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 16:17:43 +0900

    chore: 메서드명 오타 수정

--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
     private final String INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     private final String INVALID_ORDER_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
-    public int ReadDate() {
+    public int readDate() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
@@ -34,7 +34,7 @@ public class InputView {
         }
     }
 
-    public Map<String, Integer> ReadOrders() {
+    public Map<String, Integer> readOrders() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
 
