1de4c69b0104633180493ee5726de02114d9af45
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 13:39:30 +0900

    refactor: determineEventBadge 메서드 삭제

--- src/main/java/christmas/EventBadge.java
+++ src/main/java/christmas/EventBadge.java
@@ -11,17 +11,4 @@ public enum EventBadge {
     EventBadge(Integer benefitPrice) {
         this.benefitPrice = benefitPrice;
     }
-
-    public static EventBadge determineEventBadge(int totalBenefitPrice) {
-        if (totalBenefitPrice < 별.benefitPrice) {
-            return 미대상;
-        }
-        if (totalBenefitPrice < 트리.benefitPrice) {
-            return 별;
-        }
-        if (totalBenefitPrice < 산타.benefitPrice) {
-            return 트리;
-        }
-        return 산타;
-    }
 }
