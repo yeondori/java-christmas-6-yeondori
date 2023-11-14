bde8752d8981c2c57596e43e421b6813c78efe27
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 14:57:29 +0900

    refactor: 테스트 케이스 상수화

--- src/test/java/christmas/MenuBoardTest.java
+++ src/test/java/christmas/MenuBoardTest.java
@@ -8,42 +8,44 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuBoardTest {
     MenuBoard menuBoard = MenuBoard.loadMenu();
+    private static final String 있는_메뉴 = "양송이수프";
+    private static final String 없는_메뉴 = "뿌링클";
 
     @DisplayName("메뉴판에 존재하는 메뉴면 true를 반환한다.")
     @Test
     void hasMenuTrueCase() {
-        assertThat(menuBoard.hasMenu("양송이수프")).isTrue();
+        assertThat(menuBoard.hasMenu(있는_메뉴)).isTrue();
     }
 
     @DisplayName("메뉴판에 존재하지 않는 메뉴면 false를 반환한다.")
     @Test
     void hasMenuFalseCase() {
-        assertThat(menuBoard.hasMenu("뿌링클")).isFalse();
+        assertThat(menuBoard.hasMenu(없는_메뉴)).isFalse();
     }
 
     @DisplayName("메뉴판에 존재하면 메뉴를 반환한다.")
     @Test
     void findMenuIfPresent() {
-        assertThat(menuBoard.findMenu("양송이수프")).isInstanceOf(Menu.class);
+        assertThat(menuBoard.findMenu(있는_메뉴)).isInstanceOf(Menu.class);
     }
 
     @DisplayName("메뉴판에 존재하지 않으면 예외를 발생한다.")
     @Test
     void findMenuIfNotPresent() {
-        assertThatThrownBy(() -> menuBoard.findMenu("뿌링클"))
+        assertThatThrownBy(() -> menuBoard.findMenu(없는_메뉴))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("메뉴판에 존재하면 카테고리를 반환한다.")
     @Test
     void findCategoryIfPresent() {
-        assertThat(menuBoard.findCategory("양송이수프")).isEqualTo(Category.APPETIZER);
+        assertThat(menuBoard.findCategory(있는_메뉴)).isEqualTo(Category.APPETIZER);
     }
 
     @DisplayName("메뉴판에 존재하지 않으면 예외를 발생한다.")
     @Test
     void findCategoryIfNotPresent() {
-        assertThatThrownBy(() -> menuBoard.findCategory("뿌링클"))
+        assertThatThrownBy(() -> menuBoard.findCategory(없는_메뉴))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
