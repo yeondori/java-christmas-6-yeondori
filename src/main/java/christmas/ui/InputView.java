d54e1e75f5ee51fa4c1486359900f30d3ea6b939
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 16:58:58 +0900

    chore: reformat

--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 public class InputView {
 
-
     private final String INVALID_INPUT_MESSAGE = "[ERROR] 유효하지 않은 입력입니다. 다시 입력해 주세요.";
     private final String INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     private final String INVALID_ORDER_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
