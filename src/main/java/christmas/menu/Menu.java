fec2179bbd050c31964e19bb4c8943d62888cfdc
yeondori <yeonseo.jung0@gmail.com>
2023-11-15 01:41:48 +0900

    refactor: 사용하지 않는 메서드 삭제(getName)

--- src/main/java/christmas/menu/Menu.java
+++ src/main/java/christmas/menu/Menu.java
@@ -15,10 +15,6 @@ public class Menu {
         return (this.name.equals(name));
     }
 
-    public String getName() {
-        return name;
-    }
-
     public int getPrice() {
         return price;
     }
