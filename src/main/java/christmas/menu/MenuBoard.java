cbda6c77545602c2d29ad4f8bf08230b633b331a
yeondori <yeonseo.jung0@gmail.com>
2023-11-15 00:57:02 +0900

    refactor: 메뉴 설정 부분 삭제(어플리케이션으로 이동)

--- src/main/java/christmas/menu/MenuBoard.java
+++ src/main/java/christmas/menu/MenuBoard.java
@@ -4,50 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MenuBoard {
-    private final List<Menu> menuBoard = new ArrayList<>();
+    private final List<Menu> menuBoard;
 
-    private MenuBoard() {
-        initMenuBoard();
-    }
-
-    public static MenuBoard loadMenu() {
-        return new MenuBoard();
-    }
-
-    private void initMenuBoard() {
-        addAppetizer();
-        addMain();
-        addDessert();
-        addDrink();
-    }
-
-    private void addAppetizer() {
-        menuBoard.add(new Menu("양송이수프", 6_000, Category.APPETIZER));
-        menuBoard.add(new Menu("타파스", 5_500, Category.APPETIZER));
-        menuBoard.add(new Menu("시저샐러드", 8_000, Category.APPETIZER));
-    }
-
-    private void addMain() {
-        menuBoard.add(new Menu("티본스테이크", 55_000, Category.MAIN));
-        menuBoard.add(new Menu("바비큐립", 54_000, Category.MAIN));
-        menuBoard.add(new Menu("해산물파스타", 35_000, Category.MAIN));
-        menuBoard.add(new Menu("크리스마스파스타", 25_000, Category.MAIN));
-    }
-
-    private void addDessert() {
-        menuBoard.add(new Menu("초코케이크", 15_000, Category.DESSERT));
-        menuBoard.add(new Menu("아이스크림", 5_000, Category.DESSERT));
-    }
-
-    private void addDrink() {
-        menuBoard.add(new Menu("제로콜라", 3_000, Category.DRINK));
-        menuBoard.add(new Menu("레드와인", 60_000, Category.DRINK));
-        menuBoard.add(new Menu("샴페인", 25_000, Category.DRINK));
+    public MenuBoard(List<Menu> menuBoard) {
+        this.menuBoard = menuBoard;
     }
 
     public boolean hasMenu(String name) {
         return menuBoard.stream()
-                .anyMatch(menu -> menu.getName().equals(name));
+                .anyMatch(menu -> menu.isName(name));
     }
 
     private Menu findMenu(String name) {
