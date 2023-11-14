01ac6feac5f5889a6c2e73a93800bbfc8133c17d
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 16:19:51 +0900

    feat: InputViewTest 생성

---
+++ src/test/java/christmas/ui/InputViewTest.java
@@ -0,0 +1,16 @@
+package christmas.ui;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputViewTest {
+
+    @Test
+    void readDate() {
+    }
+
+    @Test
+    void readOrders() {
+    }
+}
