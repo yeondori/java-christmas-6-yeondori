cb24a90ae4e37fb7dc8edd343bcf00a37c5d0e19
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 14:10:22 +0900

    feat: getEventBadge 테스트(총혜택금액이 5천원 이상 1만원 미만인 경우)

--- src/test/java/christmas/EventControllerTest.java
+++ src/test/java/christmas/EventControllerTest.java
@@ -7,16 +7,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class EventControllerTest {
     private final int StarThreshold = 5000;
-    private final int TreeThreshold = 5000;
-    private final int SantaThreshold = 5000;
+    private final int TreeThreshold = 10000;
+    private final int SantaThreshold = 20000;
 
-    @DisplayName("총혜택금액이 5천원 미만이면 배지 미대상이다.")
+    @DisplayName("총혜택금액이 5천원 미만이면 배지 '미대상'이다.")
     @Test
     void getEventBadgeIfBenefitUnder5000() {
-        int totalBenefitPrice = SantaThreshold - 1;
+        int totalBenefitPrice = StarThreshold - 1;
         EventBadge eventBadge = EventController.getEventBadge(totalBenefitPrice);
 
         assertThat(eventBadge).isEqualTo(EventBadge.미대상);
+    }
+
+    @DisplayName("총혜택금액이 5천원 이상 1만원 미만이면 배지 '별'이다.")
+    @Test
+    void getEventBadgeIfBenefitUnder10000() {
+        int totalBenefitPrice = TreeThreshold - 1;
+        EventBadge eventBadge = EventController.getEventBadge(totalBenefitPrice);
+
+        assertThat(eventBadge).isEqualTo(EventBadge.별);
     }
 
     @Test
