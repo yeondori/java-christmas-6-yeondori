64fbfcddd676552a4fbf7fbfbffe06e9f690e785
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 17:20:23 +0900

    feat: printBenefits 기능 추가

--- src/main/java/christmas/ui/OutputView.java
+++ src/main/java/christmas/ui/OutputView.java
@@ -1,5 +1,7 @@
 package christmas.ui;
 
+import christmas.discount.Discount;
+import christmas.event.EventController;
 import christmas.order.Order;
 import christmas.order.OrderController;
 
@@ -8,11 +10,11 @@ import java.util.Map;
 
 public class OutputView {
     public void printResult() {
-        System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+        System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
     }
 
     public void printOrder(List<Order> orders) {
-        System.out.println("<주문 메뉴>");
+        System.out.println("\n<주문 메뉴>");
         Map<String, Integer> orderMenus = OrderController.getOrders(orders);
 
         orderMenus.entrySet().stream()
@@ -23,5 +25,18 @@ public class OutputView {
         System.out.println(OrderController.getTotalPrice(orders));
     }
 
-    public void print
+    public void printBenefits(List<Order> orders, int date) {
+        System.out.println("\n<증정 메뉴>");
+
+        EventController eventController = EventController.from(orders);
+        Map<Discount, Integer> totalBenefits = eventController.getTotalBenefits(date);
+
+        Integer giftDiscount = totalBenefits.get(Discount.GIFT);
+        if (giftDiscount != 0) {
+            System.out.println("샴페인 1개");
+        }
+        if (giftDiscount == 0) {
+            System.out.println("없음");
+        }
+    }
 }
