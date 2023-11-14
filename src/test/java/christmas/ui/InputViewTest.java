76156625d169e358e1543920dcdc31d1804cf420
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 16:39:58 +0900

    feat: readOrders 테스트 (유효하지 않은 주문 형식의 경우)

--- src/test/java/christmas/ui/InputViewTest.java
+++ src/test/java/christmas/ui/InputViewTest.java
@@ -2,16 +2,12 @@ package christmas.ui;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import christmas.Application;
-import christmas.order.OrderController;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest extends NsTest {
 
@@ -30,8 +26,21 @@ class InputViewTest extends NsTest {
 
     @DisplayName("주문은 한글,숫자,쉼표(,),대시(-) 이외의 값을 입력받을 수 없다.")
     @ParameterizedTest
-    @ValueSource(strings = {"t-boneSteak","티본steak-1", "티본스테이크-1, 양송이수프-5"})
+    @ValueSource(strings = {"t-boneSteak", "티본steak-1", "티본스테이크-1, 양송이수프-5"})
     void readOrdersWithInvalidInputFormat(String invalidInput) {
+        InputView inputView = new InputView();
+
+        assertSimpleTest(() -> {
+            run(invalidInput);
+            assertThatThrownBy(() -> inputView.readOrders())
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
+    }
+
+    @DisplayName("각각의 주문은 한글-대시(-)-숫자로만 구성되어야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"tboneSteak-3", "티본steak-1개", "티본스테이크-one", "티본스테이크!-4"})
+    void readOrdersWithInvalidInputOrderFormat(String invalidInput) {
         InputView inputView = new InputView();
 
         assertSimpleTest(() -> {
