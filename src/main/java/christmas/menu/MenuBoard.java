5a754f25521c878f86e1bc387ce785fa263f509a
yeondori <yeonseo.jung0@gmail.com>
2023-11-15 00:28:52 +0900

    refactor: 정적 팩터리 메서드 삭제에 따른 생성자 호출

--- src/main/java/christmas/menu/MenuBoard.java
+++ src/main/java/christmas/menu/MenuBoard.java
@@ -22,27 +22,27 @@ public class MenuBoard {
     }
 
     private void addAppetizer() {
-        menuBoard.add(Menu.of("양송이수프", 6_000, Category.APPETIZER));
-        menuBoard.add(Menu.of("타파스", 5_500, Category.APPETIZER));
-        menuBoard.add(Menu.of("시저샐러드", 8_000, Category.APPETIZER));
+        menuBoard.add(new Menu("양송이수프", 6_000, Category.APPETIZER));
+        menuBoard.add(new Menu("타파스", 5_500, Category.APPETIZER));
+        menuBoard.add(new Menu("시저샐러드", 8_000, Category.APPETIZER));
     }
 
     private void addMain() {
-        menuBoard.add(Menu.of("티본스테이크", 55_000, Category.MAIN));
-        menuBoard.add(Menu.of("바비큐립", 54_000, Category.MAIN));
-        menuBoard.add(Menu.of("해산물파스타", 35_000, Category.MAIN));
-        menuBoard.add(Menu.of("크리스마스파스타", 25_000, Category.MAIN));
+        menuBoard.add(new Menu("티본스테이크", 55_000, Category.MAIN));
+        menuBoard.add(new Menu("바비큐립", 54_000, Category.MAIN));
+        menuBoard.add(new Menu("해산물파스타", 35_000, Category.MAIN));
+        menuBoard.add(new Menu("크리스마스파스타", 25_000, Category.MAIN));
     }
 
     private void addDessert() {
-        menuBoard.add(Menu.of("초코케이크", 15_000, Category.DESSERT));
-        menuBoard.add(Menu.of("아이스크림", 5_000, Category.DESSERT));
+        menuBoard.add(new Menu("초코케이크", 15_000, Category.DESSERT));
+        menuBoard.add(new Menu("아이스크림", 5_000, Category.DESSERT));
     }
 
     private void addDrink() {
-        menuBoard.add(Menu.of("제로콜라", 3_000, Category.DRINK));
-        menuBoard.add(Menu.of("레드와인", 60_000, Category.DRINK));
-        menuBoard.add(Menu.of("샴페인", 25_000, Category.DRINK));
+        menuBoard.add(new Menu("제로콜라", 3_000, Category.DRINK));
+        menuBoard.add(new Menu("레드와인", 60_000, Category.DRINK));
+        menuBoard.add(new Menu("샴페인", 25_000, Category.DRINK));
     }
 
     public boolean hasMenu(String name) {
@@ -50,9 +50,9 @@ public class MenuBoard {
                 .anyMatch(menu -> menu.getName().equals(name));
     }
 
-    public Menu findMenu(String name) {
+    private Menu findMenu(String name) {
         return menuBoard.stream()
-                .filter(menu -> menu.getName().equals(name))
+                .filter(menu -> menu.isName(name))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("[ERROR] 해당 메뉴가 존재하지 않습니다."));
     }
