a35747af1a788d770016b278d25344cc8fc9c5fe
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 15:21:49 +0900

    feat: ReadDate, validateDate 기능 추가

--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -1,5 +1,26 @@
 package christmas.ui;
 
-public class InputView {
+import camp.nextstep.edu.missionutils.Console;
 
+public class InputView {
+    public int ReadDate() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+
+        return validateDate(input);
+    }
+
+    private int validateDate(String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+
+        int date = Integer.parseInt(input);
+
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+        return date;
+    }
 }
