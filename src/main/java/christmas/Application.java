58ae1eec526e6cff955bdcd68dd363e49f8ed8d1
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 18:38:01 +0900

    feat: requestDate 기능 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,23 @@
 package christmas;
 
+import christmas.ui.InputView;
+
+import java.util.Map;
+
 public class Application {
+    static InputView inputView = new InputView();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int date = requestDate();
+
+    }
+
+    public static int requestDate() {
+        while (true) {
+            try {
+                return inputView.readDate();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
