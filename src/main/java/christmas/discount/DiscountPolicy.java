9f5b85ee4dc4b01539c05faf7895f599a0d38d2d
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 22:44:47 +0900

    feat: 할인적용조건 추가(최소 주문 금액 1만원)

--- src/main/java/christmas/discount/DiscountPolicy.java
+++ src/main/java/christmas/discount/DiscountPolicy.java
@@ -4,36 +4,56 @@ import static christmas.discount.Discount.*;
 import static christmas.event.EventCalendar.*;
 
 public class DiscountPolicy {
+    private final int MIN_ORDER_AMOUNT_FOR_DISCOUNT = 10000;
 
-    public int getChristmasDiscount(int date) {
+    private boolean isEligibleForDiscount(int orderAmount) {
+        return orderAmount >= MIN_ORDER_AMOUNT_FOR_DISCOUNT;
+    }
+
+    public int getChristmasDiscount(int orderAmount, int date) {
+        if (!isEligibleForDiscount(orderAmount)) {
+            return 0;
+        }
         if (date >= CHRISTMAS_START && date <= CHRISTMAS_END) {
             return CHRISTMAS.getBaseDiscount() + (date - 1) * CHRISTMAS.getAdditionalDiscount();
         }
         return 0;
     }
 
-    public int getSpecialDayDiscount(int date) {
+    public int getSpecialDayDiscount(int orderAmount, int date) {
+        if (!isEligibleForDiscount(orderAmount)) {
+            return 0;
+        }
         if (SPECIAL_DAY.contains(date)) {
             return SPECIAL.getBaseDiscount();
         }
         return 0;
     }
 
-    public int getWeekdayDiscount(int date, int dessertQuantity) {
+    public int getWeekdayDiscount(int orderAmount, int date, int dessertQuantity) {
+        if (!isEligibleForDiscount(orderAmount)) {
+            return 0;
+        }
         if (!WEEKEND_DAY.contains(date)) {
             return dessertQuantity * WEEKDAY.getBaseDiscount();
         }
         return 0;
     }
 
-    public int getWeekendDiscount(int date, int mainQuantity) {
+    public int getWeekendDiscount(int orderAmount, int date, int mainQuantity) {
+        if (!isEligibleForDiscount(orderAmount)) {
+            return 0;
+        }
         if (WEEKEND_DAY.contains(date)) {
             return mainQuantity * WEEKEND.getBaseDiscount();
         }
         return 0;
     }
 
-    public int getGiftDiscount(int totalPrice) {
+    public int getGiftDiscount(int orderAmount, int totalPrice) {
+        if (!isEligibleForDiscount(orderAmount)) {
+            return 0;
+        }
         if (totalPrice >= GIFT.getPriceCondition()) {
             return GIFT.getBaseDiscount();
         }
