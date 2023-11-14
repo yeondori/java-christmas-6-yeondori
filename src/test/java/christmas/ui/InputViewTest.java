cde029ce1f63df18da37cc1c1f994498db64762e
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 16:34:08 +0900

    feat: readDate 테스트 (유효하지 않은 입력의 경우)

--- src/test/java/christmas/ui/InputViewTest.java
+++ src/test/java/christmas/ui/InputViewTest.java
@@ -1,16 +1,39 @@
 package christmas.ui;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.Application;
+import christmas.order.OrderController;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
-class InputViewTest {
+class InputViewTest extends NsTest {
 
-    @Test
-    void readDate() {
+    @DisplayName("날짜에 숫자 이외의 값을 입력하면 예외 처리한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"12일", "12th", "friday", "!@#"})
+    void readDateWithInvaildInput(String invalidInput) {
+        InputView inputView = new InputView();
+
+        assertSimpleTest(() -> {
+            run(invalidInput);
+            assertThatThrownBy(() -> inputView.readDate())
+                    .isInstanceOf(IllegalArgumentException.class);
+        });
     }
 
     @Test
     void readOrders() {
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
