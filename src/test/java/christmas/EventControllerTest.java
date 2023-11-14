02436ece28a2f5190bdd96c59457250d95a5c296
yeondori <yeonseo.jung0@gmail.com>
2023-11-14 14:09:16 +0900

    feat: getEventBadge 테스트(총혜택금액이 5천원 미만인 경우)

--- src/test/java/christmas/EventControllerTest.java
+++ src/test/java/christmas/EventControllerTest.java
@@ -1,13 +1,22 @@
 package christmas;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class EventControllerTest {
+    private final int StarThreshold = 5000;
+    private final int TreeThreshold = 5000;
+    private final int SantaThreshold = 5000;
 
+    @DisplayName("총혜택금액이 5천원 미만이면 배지 미대상이다.")
     @Test
-    void getEventBadge() {
+    void getEventBadgeIfBenefitUnder5000() {
+        int totalBenefitPrice = SantaThreshold - 1;
+        EventBadge eventBadge = EventController.getEventBadge(totalBenefitPrice);
+
+        assertThat(eventBadge).isEqualTo(EventBadge.미대상);
     }
 
     @Test
